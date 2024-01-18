5808b7346a4e47ad99b25f3bf591dcf86fca8f27
Aseem Mangla <aseem.mangla@ukg.com>
2024-01-18 15:44:48 +0530

    Scheduling Oracle
    Signed-off-by: Aseem Mangla <aseem.mangla@ukg.com>

--- src/CurlComponent.js
+++ src/CurlComponent.js
@@ -13,14 +13,15 @@ const CurlComponent = ({setOutputData, setValue}) => {
   });
 
   const handleChange = (e) => {
-    setOutputData('')
+    setOutputData('');
+    setValue(0);
     setInputData({ ...inputData, [e.target.name]: e.target.value });
   };
 
   const handleFetchCurl = async () => {
     const { Tenant, WorkCode, Year, Month } = inputData;
     const headers = {
-        'Authorization': 'Bearer ya29.a0AfB_byBWVIBVXJ0L8wxgE98W20hr_Y6aj8pD91inqIve3PmVqqt_uiXzmUcf7OkbOe93fqhZyjvYvSVqwy9xme98geTGe1ohKPSVezkDO1GVhnIyIkRmA4sBMFhWFQPaX2LOrl-vJzyCFl_jLqLzOH-f39iG-7O5MsaIcSOM5LsaCgYKAcESAQ8SFQHGX2MicDomw5dMK2evB4HqhqocMA0178',
+        'Authorization': 'Bearer ya29.a0AfB_byAzDywLnZ_IfbtvJOHkZxK1Os3E5H5jpckO9O6qHfs-GwBcsAcVQ5sU2rlw7bVH9ZK7TlowANhoSdQ-cWOmiRAuQiUbTx-jsze50EUR_X8eVMMMqN6qxF0gEyGITVst2tWK8vkqzDyW5Kz1bZBhDLbnp7u8FrYMF9pgPQaCgYKAc4SAQ8SFQHGX2MigxjXf_DXEMBOmdHIvy8Oiw0177',
         'Content-Type': 'application/json'
       };
       const data = {
@@ -33,7 +34,7 @@ const CurlComponent = ({setOutputData, setValue}) => {
           }
         ]
       };
-      axios.post('https://us-central1-aiplatform.googleapis.com/v1/projects/541554200995/locations/us-central1/endpoints/5710469769505800192:predict', data, { headers })
+      await axios.post('https://us-central1-aiplatform.googleapis.com/v1/projects/541554200995/locations/us-central1/endpoints/5710469769505800192:predict', data, { headers })
         .then((response) => {
         console.log(response.data);
           setOutputData(JSON.stringify(response.data, null, 2));
@@ -44,28 +45,28 @@ const CurlComponent = ({setOutputData, setValue}) => {
         });
   };
 
-  
+  console.log(inputData);
   return (
     <Container className="mt-5">
-      <Form className="card p-4">
+      <Form className="card p-4" >
         <h2 className="mb-4">Input Parameters</h2>
         <Form.Group className="mb-3">
           <Form.Label>Tenant</Form.Label>
-          <Form.Control type="text" name="Tenant" value={inputData.Tenant} onChange={handleChange} />
+          <Form.Control required={true} type="text" name="Tenant" value={inputData.Tenant} onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Scheduling Type</Form.Label>
-          <Form.Control type="text" name="WorkCode" value={inputData.WorkCode} onChange={handleChange} />
+          <Form.Control required={true} type="text" name="WorkCode" value={inputData.WorkCode} onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Year</Form.Label>
-          <Form.Control type="text" name="Year" value={inputData.Year} onChange={handleChange} />
+          <Form.Control required={true} type="number" placeholder='Please Enter a Number' name="Year" value={inputData.Year} onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Month</Form.Label>
-          <Form.Control type="text" name="Month" value={inputData.Month} onChange={handleChange} />
+          <Form.Control required={true} type="text" name="Month" value={inputData.Month} onChange={handleChange} />
         </Form.Group>
-        <Button style={{backgroundColor: '#005151'}} variant="success" onClick={handleFetchCurl}>See Predictions</Button>
+        <Button style={{backgroundColor: '#005151'}} onClick={handleFetchCurl}>See Predictions</Button>
       </Form>
     </Container>
   );
